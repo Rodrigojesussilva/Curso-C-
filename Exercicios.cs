ff3e395e3b6a1f22c96d9712565cd74740151193
Rodrigo <rodrigo.jesussilva@hotmail.com>
2024-08-27 14:03:59 -0300

    update

--- Exercicios.cs
+++ Exercicios.cs
@@ -2,209 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Curso_C_
 {
-    public class Exercicios
+   public class Advogado
     {
+        public string nome;
+        public string oab;
+        public double salario;
 
-        /* public void MostrarResultado()
-         {
-
-             int[] numeros = { 2, 4, 6 , 8 ,10 };
-             for (int i = 0; i < numeros.Length; i++)
-             {
-                 Console.WriteLine(numeros[i]);
-             }
-
-
-         }
-         public void MostrarResultado()
+        public void ExibirInformacoes()
         {
-
-            int[] numeros = { 1, 3, 5, 7, 9 };
-            Console.WriteLine(numeros[3]);
-
-
+            Console.WriteLine($"O nome do advogado é {nome}\nSeu oab é {oab}\nSeu salário é {salario}");
         }
-
-        public void MostrarResultado()
-        {
-            string[] nomes = {"Ana", "Bruno", "Carla" };
-            for (int i = 0; i < nomes.Length; i++) 
-            {
-                if (nomes[i] == "Bruno") 
-                {
-                    nomes[i] = "Beto";  
-                }
-            
-            }
-            for (int i = 0; i < nomes.Length; i++)
-            {
-                Console.WriteLine(nomes[i]);
-
-            }
-        }
-
-        public void MostrarResultado()
-        {
-            int[] notas = { 7, 8, 9, 10, 6 };
-            int soma = 0;
-            for (int i = 0; i < notas.Length; i++) 
-            {
-                soma += notas[i];
-            
-            }
-            Console.WriteLine(soma);
-        }
-
-         public void MostrarResultado()
-        {
-            int[] numeros = { 5, 10, 15, 20, 25 };
-            
-            for (int i = 0; i < numeros.Length; i++) 
-            {
-               if (numeros[i] == 15) 
-                {
-                    Console.WriteLine($"o VALOR 15 ESTA PRESENTE");
-                }
-            
-            }          
-        }
-
-        public void MostrarResultado()
-        {
-           List<string> frutas = new List<string>();
-            frutas.Add("Maça");
-            frutas.Add("Laranja");
-            frutas.Add("Banana");
-
-            foreach (string s in frutas) 
-            {
-                Console.WriteLine(s);
-            }
-
-        }
-
-         public void MostrarResultado()
-        {
-           List<int> numeros = new List<int>();
-            numeros.Add(1);
-            numeros.Add(2);
-            numeros.Add(3);
-            numeros.Add(4);
-            numeros.Add(5);
-
-            numeros.RemoveAt(2);
-
-            foreach (int numero in numeros) 
-            {
-                Console.WriteLine(numero);            
-            }
-            
-
-        }
-
-         public void MostrarResultado()
-        {
-            List<string> cidades = new List<string>();
-            cidades.Add("Sao Paulo");
-            cidades.Add("Rio de janeiro");
-            cidades.Add("Belo Horizonte");
-
-            Console.WriteLine(cidades[1]);
-            
-
-        }
-
-         public void MostrarResultado()
-        {
-            List<string> animais = new List<string>();
-            animais.Add("Cachorro");
-            animais.Add("Gato");
-            animais.Add("Coelho");
-
-            Console.WriteLine($"A lista contem {animais.Count} elementos");
-
-        }
-
-          public void CalculaSoma(int n, int n2)
-        {
-            int soma = n + n2;        
-        }
-
-        string marca = "Toyota";
-
-        public void Mostrar()
-        {
-            Console.WriteLine(marca);
-        } 
-
-        string nome = "";
-
-        public void Mostrar() 
-        {
-            string nome = "";
-            Console.WriteLine(nome);
-            Console.WriteLine(this.nome);
-
-        }
-
-        public void Mostrar() 
-        {
-          
-            if (true) 
-            {
-                int x = 0;
-            }
-
-           // Console.WriteLine(x);
-        }
-
-         string nome = "";
-
-        public void Mostrar() 
-        {
-            string nome = "";
-            Console.WriteLine(nome);
-            Console.WriteLine(this.nome);
-
-        }
-
-        public void Mostrar()
-        {
-            try
-            {
-                string nome = "Rodrigo";
-                int numParse = int.Parse(nome); // Isso lançará uma exceção
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message); // Este bloco será executado
-            }
-        }
-
-         public void Mostrar()
-        {
-            try
-            {
-                int n = -1;
-                if (n < 0)
-                {
-                    throw new Exception("Erro personalizado: Numero negativo.");
-                }
-              
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message); // Este bloco será executado
-            }
-        }
-
-        */
-       
     }
-}
+    
+ }
--- Program.cs
+++ Program.cs
@@ -1,30 +1,8 @@
 ﻿using Curso_C_;
 
+Advogado advogado = new Advogado();
+advogado.oab = "asdadas";
+advogado.salario = 15.000;
+advogado.nome = "Cantoara";
 
-
-//ConceitosPOO conceitos = new ConceitosPOO();
-
-// Explicar Orientação a Objetos
-//conceitos.ExplicarOrientacaoAObjetos();
-
-// Explicar a diferença entre Programação Estruturada e Orientada a Objetos
-//conceitos.DiferencaEntreParadigmas();
-
-// Explicar os Pilares da Orientação a Objetos
-//conceitos.ExplicarPilares();
-// 1. Criando Objetos (Instanciando a classe Pessoa)
-
-Pessoa pessoa1 = new Pessoa("Alice", 30);
-Pessoa pessoa2 = new Pessoa("Bob", 25);
-
-// 2. Usando Métodos de Instância (Métodos não estáticos)
-pessoa1.ExibirInformacoes();
-pessoa2.ExibirInformacoes();
-
-// 3. Usando Método Estático (Pertence à Classe, não à instância)
-Pessoa.ExibirNumeroDePessoas();
-
-// 4. Membros de Instância vs. Membros Estáticos
-Console.WriteLine($"Pessoa1: {pessoa1.Nome}, Idade: {pessoa1.Idade}");
-Console.WriteLine($"Pessoa2: {pessoa2.Nome}, Idade: {pessoa2.Idade}");
-Console.WriteLine($"Número total de pessoas: {Pessoa.NumeroDePessoas}");
+advogado.ExibirInformacoes();
